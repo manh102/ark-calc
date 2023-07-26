1352f108931c2838e5a22067ed1afb7e5e4486d3
PURESTREAM\trant <manh.tran@autoserver.vn>
2023-07-26 15:57:08 +0700

    # add home button temp file

---
+++ lib/features/home/presentation/widgets/avatar_button.dart
@@ -0,0 +1,8 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:new_ark_calc/core/resources/resources.dart';
+
+Widget avatarButton(String url) {
+  return Container();
+}
